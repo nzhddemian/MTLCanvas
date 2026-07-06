c845ab72273d2046cf65a803618562dd685d748d
Nezhdanov Demian <48530701+nzhddemian@users.noreply.github.com>
2026-07-05 23:46:07 -0300

    Initial Commit

---
+++ MTLCanvas/MTLCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTLCanvasApp.swift
+//  MTLCanvas
+//
+//  Created by demian on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MTLCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
